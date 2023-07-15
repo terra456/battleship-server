3a5a200ad720ad50da8d34888c781e73e9c9a135
terra456 <terra.456@yandex.ru>
2023-07-15 13:45:07 +0500

    feat: add tests

---
+++ src/ws_server/tests/Game.test.ts
@@ -0,0 +1,243 @@
+import { ShipOnClient } from "ws_server/types";
+import Game, { Ship } from "../Game";
+
+const shipsSecond: ShipOnClient[] = [
+  {position:{x:7,y:3},direction:true,type:"huge",length:4},
+  {position:{x:2,y:1},direction:true,type:"large",length:3},
+  {position:{x:5,y:3},direction:true,type:"large",length:3},
+  {position:{x:1,y:7},direction:true,type:"medium",length:2},
+  {position:{x:5,y:9},direction:false,type:"medium",length:2},
+  {position:{x:8,y:8},direction:false,type:"medium",length:2},
+  {position:{x:3,y:8},direction:false,type:"small",length:1},
+  {position:{x:6,y:0},direction:true,type:"small",length:1},
+  {position:{x:1,y:5},direction:true,type:"small",length:1},
+  {position:{x:4,y:0},direction:true,type:"small",length:1},
+];
+
+const shipsFirst: ShipOnClient[] = [
+  {position:{x:1,y:6},direction:true,type:"huge",length:4},
+  {position:{x:3,y:4},direction:true,type:"large",length:3},
+  {position:{x:9,y:7},direction:true,type:"large",length:3},
+  {position:{x:3,y:8},direction:true,type:"medium",length:2},
+  {position:{x:5,y:7},direction:true,type:"medium",length:2},
+  {position:{x:7,y:7},direction:true,type:"medium",length:2},
+  {position:{x:1,y:4},direction:true,type:"small",length:1},
+  {position:{x:5,y:5},direction:true,type:"small",length:1},
+  {position:{x:7,y:5},direction:true,type:"small",length:1},
+  {position:{x:9,y:4},direction:true,type:"small",length:1},
+];
+
+const shipsSecond1: Ship[] = [
+  {position:{x:7,y:3},direction:true,type:"huge",length:4,
+    status: "unbroken", fields: [{x:7,y:3,shot:false}, {x:7,y:4,shot:false}, {x:7,y:5,shot:false}, {x:7,y:6,shot:false}]},
+  {position:{x:2,y:1},direction:true,type:"large",length:3,
+    status: "unbroken", fields: [{x:2,y:1,shot:false}, {x:2,y:2,shot:false}, {x:2,y:3,shot:false}]},
+  {position:{x:5,y:3},direction:true,type:"large",length:3,
+    status: "unbroken", fields: [{x:5,y:3,shot:false}, {x:5,y:4,shot:false}, {x:5,y:5,shot:false}]},
+  {position:{x:1,y:7},direction:true,type:"medium",length:2,
+    status: "unbroken", fields: [{x:1,y:7,shot:false}, {x:1,y:8,shot:false}]},
+  {position:{x:5,y:9},direction:false,type:"medium",length:2,
+    status: "unbroken", fields: [{x:5,y:9,shot:false}, {x:6,y:9,shot:false}]},
+  {position:{x:8,y:8},direction:false,type:"medium",length:2,
+    status: "unbroken", fields: [{x:8,y:8,shot:false}, {x:9,y:8,shot:false}]},
+  {position:{x:3,y:8},direction:false,type:"small",length:1,
+    status: "unbroken", fields: [{x:3,y:8,shot:false}]},
+  {position:{x:6,y:0},direction:true,type:"small",length:1,
+    status: "unbroken", fields: [{x:6,y:0,shot:false}]},
+  {position:{x:1,y:5},direction:true,type:"small",length:1,
+    status: "unbroken", fields: [{x:1,y:5,shot:false}]},
+  {position:{x:4,y:0},direction:true,type:"small",length:1,
+    status: "unbroken", fields: [{x:4,y:0,shot:false}]},
+];
+const shipsSecond2: Ship[] = [
+  {position:{x:7,y:3},direction:true,type:"huge",length:4,
+    status: "unbroken", fields: [{x:7,y:3,shot:false}, {x:7,y:4,shot:false}, {x:7,y:5,shot:false}, {x:7,y:6,shot:false}]},
+  {position:{x:2,y:1},direction:true,type:"large",length:3,
+    status: "unbroken", fields: [{x:2,y:1,shot:false}, {x:2,y:2,shot:false}, {x:2,y:3,shot:false}]},
+  {position:{x:5,y:3},direction:true,type:"large",length:3,
+    status: "unbroken", fields: [{x:5,y:3,shot:false}, {x:5,y:4,shot:false}, {x:5,y:5,shot:false}]},
+  {position:{x:1,y:7},direction:true,type:"medium",length:2,
+    status: "shot", fields: [{x:1,y:7,shot:false}, {x:1,y:8,shot:true}]},
+  {position:{x:5,y:9},direction:false,type:"medium",length:2,
+    status: "unbroken", fields: [{x:5,y:9,shot:false}, {x:6,y:9,shot:false}]},
+  {position:{x:8,y:8},direction:false,type:"medium",length:2,
+    status: "unbroken", fields: [{x:8,y:8,shot:false}, {x:9,y:8,shot:false}]},
+  {position:{x:3,y:8},direction:false,type:"small",length:1,
+    status: "unbroken", fields: [{x:3,y:8,shot:false}]},
+  {position:{x:6,y:0},direction:true,type:"small",length:1,
+    status: "unbroken", fields: [{x:6,y:0,shot:false}]},
+  {position:{x:1,y:5},direction:true,type:"small",length:1,
+    status: "unbroken", fields: [{x:1,y:5,shot:false}]},
+  {position:{x:4,y:0},direction:true,type:"small",length:1,
+    status: "unbroken", fields: [{x:4,y:0,shot:false}]},
+];
+const shipsSecond3: Ship[] = [
+  {position:{x:7,y:3},direction:true,type:"huge",length:4,
+    status: "unbroken", fields: [{x:7,y:3,shot:false}, {x:7,y:4,shot:false}, {x:7,y:5,shot:false}, {x:7,y:6,shot:false}]},
+  {position:{x:2,y:1},direction:true,type:"large",length:3,
+    status: "unbroken", fields: [{x:2,y:1,shot:false}, {x:2,y:2,shot:false}, {x:2,y:3,shot:false}]},
+  {position:{x:5,y:3},direction:true,type:"large",length:3,
+    status: "unbroken", fields: [{x:5,y:3,shot:false}, {x:5,y:4,shot:false}, {x:5,y:5,shot:false}]},
+  {position:{x:1,y:7},direction:true,type:"medium",length:2,
+    status: "killed", fields: [{x:1,y:7,shot:true}, {x:1,y:8,shot:true}]},
+  {position:{x:5,y:9},direction:false,type:"medium",length:2,
+    status: "unbroken", fields: [{x:5,y:9,shot:false}, {x:6,y:9,shot:false}]},
+  {position:{x:8,y:8},direction:false,type:"medium",length:2,
+    status: "unbroken", fields: [{x:8,y:8,shot:false}, {x:9,y:8,shot:false}]},
+  {position:{x:3,y:8},direction:false,type:"small",length:1,
+    status: "unbroken", fields: [{x:3,y:8,shot:false}]},
+  {position:{x:6,y:0},direction:true,type:"small",length:1,
+    status: "unbroken", fields: [{x:6,y:0,shot:false}]},
+  {position:{x:1,y:5},direction:true,type:"small",length:1,
+    status: "unbroken", fields: [{x:1,y:5,shot:false}]},
+  {position:{x:4,y:0},direction:true,type:"small",length:1,
+    status: "unbroken", fields: [{x:4,y:0,shot:false}]},
+];
+
+beforeAll(() => {
+  const game = undefined;
+});
+
+
+describe('Test play with 2 users', () => {
+  const game = new Game(3, 0,'Test1');
+  
+  test('Should create game with parameters', () => {
+    expect(game.roomId).toBe(3);
+    expect(game.roomUsers).toHaveLength(1);
+    expect(game.isGame).toBe(false);
+    // expect(game.currentPlayer).toBe(0);
+    // expect(game.isWithBot).toBe(false);
+  })
+
+  test('add second player', () => {
+    game.addPlayer(1, 'Test2');
+    expect(game.roomUsers).toHaveLength(2);
+    expect(game.isGame).toBe(true);
+  })
+
+  test('add ships to first player', () => {
+    game.addShips(0, shipsFirst);
+    expect(game.roomUsers[0].ships).toHaveLength(10);
+    
+  })
+
+  test('add ships to second player', () => {
+    expect(game.roomUsers[1].ships).toBeUndefined();
+    game.addShips(1, shipsSecond);
+    expect(game.roomUsers[1].ships).toStrictEqual(shipsSecond1);
+    // expect(game.currentPlayer).toBe(0);
+  })
+
+  test('Shoot first miss', () => {
+    game.atack({ x:3, y:5, indexPlayer:0 });
+    expect(game.roomUsers[0].shoots).toStrictEqual([{ x:3, y:5}]);
+    expect(game.roomUsers[1].shoots).toStrictEqual([]);
+    // expect(game.currentPlayer).toBe(1);
+  })
+
+  test('Shoot second miss', () => {
+    game.atack({ x:0, y:0, indexPlayer:1 });
+    expect(game.roomUsers[0].shoots).toStrictEqual([{ x:3, y:5}]);
+    expect(game.roomUsers[1].shoots).toStrictEqual([{ x:0, y:0}]);
+    // expect(game.currentPlayer).toBe(0);
+  })
+
+  test('Shoot 2 first miss', () => {
+    // expect(game.currentPlayer).toBe(0);
+    game.atack({ x:4, y:6, indexPlayer:0 });
+    expect(game.roomUsers[0].shoots).toStrictEqual([{ x:3, y:5}, { x:4, y:6}]);
+    // expect(game.currentPlayer).toBe(1);
+  })
+
+  test('Shoot 2 second miss', () => {
+    game.atack({ x:0, y:1, indexPlayer:1 });
+    console.log(game.roomUsers);
+    expect(game.roomUsers[1].shoots).toStrictEqual([{ x:0, y:0}, { x:0, y:1}]);
+    // expect(game.currentPlayer).toBe(0);
+  })
+
+  test('Shoot 3 first shot', () => {
+    // expect(game.currentPlayer).toBe(0);
+    game.atack({ x:1, y:8, indexPlayer:0 });
+    expect(game.roomUsers[0].shoots).toStrictEqual([{ x:3, y:5}, { x:4, y:6}, { x:1, y:8}]);
+    expect(game.roomUsers[1].ships).toStrictEqual(shipsSecond2);
+    // expect(game.currentPlayer).toBe(1);
+  })
+
+  test('Shoot 3 second miss', () => {
+    game.atack({ x:0, y:2, indexPlayer:1 });
+    console.log(game.roomUsers);
+    expect(game.roomUsers[1].shoots).toStrictEqual([{ x:0, y:0}, { x:0, y:1}, { x:0, y:2}]);
+    // expect(game.currentPlayer).toBe(0);
+  })
+
+  // test('Shoot 4a first repeat in the same field', () => {
+  //   // expect(game.currentPlayer).toBe(0);
+  //   expect(() => game.atack({ x:3, y:5, indexPlayer:0 })).toThrow(
+  //     new Error('Shoot in same field'),
+  //   );
+  //   expect(game.roomUsers[0].shoots).toStrictEqual([{ x:3, y:5}, { x:4, y:6}, { x:1, y:8}]);
+  //   // expect(game.currentPlayer).toBe(0);
+  // })
+
+  test('Shoot 4 first miss', () => {
+    // expect(game.currentPlayer).toBe(0);
+    game.atack({ x:1, y:9, indexPlayer:0 });
+    expect(game.roomUsers[0].shoots).toStrictEqual([{ x:3, y:5}, { x:4, y:6}, { x:1, y:8}, { x:1, y:9}]);
+    expect(game.roomUsers[1].ships).toStrictEqual(shipsSecond2);
+    // expect(game.currentPlayer).toBe(1);
+  })
+
+
+  test('Shoot 4 second miss', () => {
+    game.atack({ x:0, y:3, indexPlayer:1 });
+    console.log(game.roomUsers);
+    expect(game.roomUsers[1].shoots).toStrictEqual([{ x:0, y:0}, { x:0, y:1}, { x:0, y:2}, { x:0, y:3}]);
+    // expect(game.currentPlayer).toBe(0);
+  })
+
+  test('Shoot 5 first miss', () => {
+    // expect(game.currentPlayer).toBe(0);
+    game.atack({ x:0, y:8, indexPlayer:0 });
+    expect(game.roomUsers[0].shoots).toStrictEqual([{ x:3, y:5}, { x:4, y:6}, { x:1, y:8}, { x:1, y:9}, { x:0, y:8}]);
+    expect(game.roomUsers[1].ships).toStrictEqual(shipsSecond2);
+    // expect(game.currentPlayer).toBe(1);
+  })
+
+  test('Shoot 5 second miss', () => {
+    game.atack({ x:0, y:4, indexPlayer:1 });
+    console.log(game.roomUsers);
+    expect(game.roomUsers[1].shoots).toStrictEqual([{ x:0, y:0}, { x:0, y:1}, { x:0, y:2}, { x:0, y:3}, { x:0, y:4}]);
+    // expect(game.currentPlayer).toBe(0);
+  })
+
+  test('Shoot 6 first kill', () => {
+    // expect(game.currentPlayer).toBe(0);
+    game.atack({ x:1, y:7, indexPlayer:0 });
+    expect(game.roomUsers[0].shoots).toStrictEqual([{ x:3, y:5}, { x:4, y:6}, { x:1, y:8}, { x:1, y:9}, { x:0, y:8}, { x:1, y:7},
+      { x: 0, y: 6 },
+      { x: 0, y: 7 },
+      { x: 0, y: 8 },
+      { x: 0, y: 9 },
+      { x: 1, y: 6 },
+      { x: 1, y: 7 },
+      { x: 1, y: 8 },
+      { x: 1, y: 9 },
+      { x: 2, y: 6 },
+      { x: 2, y: 7 },
+      { x: 2, y: 8 },
+      { x: 2, y: 9 }]);
+    expect(game.roomUsers[1].ships).toStrictEqual(shipsSecond3);
+    // expect(game.currentPlayer).toBe(1);
+  })
+
+  test('Shoot 6 second miss', () => {
+    game.atack({ x:0, y:5, indexPlayer:1 });
+    console.log(game.roomUsers);
+    expect(game.roomUsers[1].shoots).toStrictEqual([{ x:0, y:0}, { x:0, y:1}, { x:0, y:2}, { x:0, y:3}, { x:0, y:4}, { x:0, y:5}]);
+    // expect(game.currentPlayer).toBe(0);
+  })
+
+
+})
---
+++ src/ws_server/tests/bot.test.ts
@@ -0,0 +1,89 @@
+// import { randomInteger } from "ws_server/utils";
+import { Ship } from "ws_server/Room";
+import { checkShipOnField } from "../game_functions";
+import { Coordinates, ShipOnClient } from "ws_server/types";
+
+const fieldCells = [
+  {x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2}, {x: 0, y: 3}, {x: 0, y: 4}, {x: 0, y: 5}, {x: 0, y: 6}, {x: 0, y: 7}, {x: 0, y: 8}, {x: 0, y: 9},
+  {x: 1, y: 0}, {x: 1, y: 1}, {x: 1, y: 2}, {x: 1, y: 3}, {x: 1, y: 4}, {x: 1, y: 5}, {x: 1, y: 6}, {x: 1, y: 7}, {x: 1, y: 8}, {x: 1, y: 9},
+  {x: 2, y: 0}, {x: 2, y: 1}, {x: 2, y: 2}, {x: 2, y: 3}, {x: 2, y: 4}, {x: 2, y: 5}, {x: 2, y: 6}, {x: 2, y: 7}, {x: 2, y: 8}, {x: 2, y: 9},
+  {x: 3, y: 0}, {x: 3, y: 1}, {x: 3, y: 2}, {x: 3, y: 3}, {x: 3, y: 4}, {x: 3, y: 5}, {x: 3, y: 6}, {x: 3, y: 7}, {x: 3, y: 8}, {x: 3, y: 9},
+  {x: 4, y: 0}, {x: 4, y: 1}, {x: 4, y: 2}, {x: 4, y: 3}, {x: 4, y: 4}, {x: 4, y: 5}, {x: 4, y: 6}, {x: 4, y: 7}, {x: 4, y: 8}, {x: 4, y: 9},
+  {x: 5, y: 0}, {x: 5, y: 1}, {x: 5, y: 2}, {x: 5, y: 3}, {x: 5, y: 4}, {x: 5, y: 5}, {x: 5, y: 6}, {x: 5, y: 7}, {x: 5, y: 8}, {x: 5, y: 9},
+  {x: 6, y: 0}, {x: 6, y: 1}, {x: 6, y: 2}, {x: 6, y: 3}, {x: 6, y: 4}, {x: 6, y: 5}, {x: 6, y: 6}, {x: 6, y: 7}, {x: 6, y: 8}, {x: 6, y: 9},
+  {x: 7, y: 0}, {x: 7, y: 1}, {x: 7, y: 2}, {x: 7, y: 3}, {x: 7, y: 4}, {x: 7, y: 5}, {x: 7, y: 6}, {x: 7, y: 7}, {x: 7, y: 8}, {x: 7, y: 9},
+  {x: 8, y: 0}, {x: 8, y: 1}, {x: 8, y: 2}, {x: 8, y: 3}, {x: 8, y: 4}, {x: 8, y: 5}, {x: 8, y: 6}, {x: 8, y: 7}, {x: 8, y: 8}, {x: 8, y: 9},
+  {x: 9, y: 0}, {x: 9, y: 1}, {x: 9, y: 2}, {x: 9, y: 3}, {x: 9, y: 4}, {x: 9, y: 5}, {x: 9, y: 6}, {x: 9, y: 7}, {x: 9, y: 8}, {x: 9, y: 9},
+]
+const fieldCellsFalse: Array<Coordinates | false> = [
+  {x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2}, {x: 0, y: 3}, {x: 0, y: 4}, {x: 0, y: 5}, {x: 0, y: 6}, {x: 0, y: 7}, {x: 0, y: 8}, {x: 0, y: 9},
+  {x: 1, y: 0}, {x: 1, y: 1}, {x: 1, y: 2}, {x: 1, y: 3}, {x: 1, y: 4}, {x: 1, y: 5}, {x: 1, y: 6}, {x: 1, y: 7}, {x: 1, y: 8}, {x: 1, y: 9},
+  {x: 2, y: 0}, {x: 2, y: 1}, {x: 2, y: 2}, {x: 2, y: 3}, {x: 2, y: 4}, {x: 2, y: 5}, {x: 2, y: 6}, {x: 2, y: 7}, {x: 2, y: 8}, {x: 2, y: 9},
+  {x: 3, y: 0}, {x: 3, y: 1}, {x: 3, y: 2}, {x: 3, y: 3}, {x: 3, y: 4}, {x: 3, y: 5}, {x: 3, y: 6}, {x: 3, y: 7}, {x: 3, y: 8}, {x: 3, y: 9},
+  {x: 4, y: 0}, {x: 4, y: 1}, {x: 4, y: 2}, {x: 4, y: 3}, {x: 4, y: 4}, {x: 4, y: 5}, {x: 4, y: 6}, {x: 4, y: 7}, {x: 4, y: 8}, {x: 4, y: 9},
+  false, false, false, false, false, false, false, false, false, false,
+  false, false, false, false, false, false, false, false, false, false,
+  false, false, false, false, false, false, false, false, false, false,
+  false, false, false, false, false, false, false, false, false, false,
+  false, false, false, false, false, false, false, false, false, false,
+]
+
+// {position:{x:7,y:3},direction:true,type:"huge",length:4},
+//   {position:{x:2,y:1},direction:true,type:"large",length:3},
+//   {position:{x:5,y:3},direction:true,type:"large",length:3},
+//   {position:{x:1,y:7},direction:true,type:"medium",length:2},
+//   {position:{x:5,y:9},direction:false,type:"medium",length:2},
+//   {position:{x:8,y:8},direction:false,type:"medium",length:2},
+//   {position:{x:3,y:8},direction:false,type:"small",length:1},
+//   {position:{x:6,y:0},direction:true,type:"small",length:1},
+//   {position:{x:1,y:5},direction:true,type:"small",length:1},
+//   {position:{x:4,y:0},direction:true,type:"small",length:1},
+
+const ships: Array<{ship: ShipOnClient, expected: Boolean}> = [
+  { ship: {position:{x:7,y:3},direction:true,type:"huge",length:4},
+    expected: false
+  },
+  { ship: {position:{x:2,y:1},direction:true,type:"large",length:3},
+    expected: true
+  },
+  { ship: {position:{x:5,y:3},direction:true,type:"large",length:3},
+    expected: false
+  },
+  { ship: {position:{x:1,y:7},direction:true,type:"medium",length:2},
+    expected: true
+  },
+  { ship: {position:{x:5,y:9},direction:false,type:"medium",length:2},
+    expected: false
+  },
+  { ship: {position:{x:8,y:8},direction:false,type:"medium",length:2},
+    expected: false
+  },
+  { ship: {position:{x:3,y:8},direction:false,type:"small",length:1},
+    expected: true
+  },
+  { ship: {position:{x:6,y:0},direction:true,type:"small",length:1},
+    expected: false
+  },
+  { ship: {position:{x:1,y:5},direction:true,type:"small",length:1},
+    expected: true
+  },
+  { ship: {position:{x:4,y:0},direction:true,type:"small",length:1},
+    expected: false
+  },
+]
+
+// describe('Random ship', () => {
+//   test('new ship 4, true', () => {
+//     expect(checkShipOnField(ships[0], fieldCellsFalse)).toBe(false);
+//   })
+// })
+
+describe.each(ships)('add new ship on field', ({ ship, expected }) => {
+  test(`get ${ship.position.x}`, () => {
+    expect(checkShipOnField(ship, fieldCellsFalse)).toBe(expected);
+  });
+
+  test(`get no false ${ship.position.x}`, () => {
+    expect(checkShipOnField(ship, fieldCells)).toBe(true);
+  });
+});
